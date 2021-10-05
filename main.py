cb754e3f5de8d12a061abd090022814a49af1dad
tma4683 <90045981+tma4683@users.noreply.github.com>
2021-10-05 21:04:17 +0400

    wrote tests for mesure pulse and abnormal functions

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pytest
 import turtle
 def read_pulse():
-    pulse1 = input("enter first pulse ")
-    pulse2 = input("enter second pulse ")
-    pulse3 = input("enter third pulse ")
+    pulse1 = int(input("enter first pulse "))
+    pulse2 = int(input("enter second pulse "))
+    pulse3 = int(input("enter third pulse "))
     return pulse1, pulse2, pulse3
 
 def measure_pulse(pulse_1,pulse_2,pulse_3):
@@ -70,6 +70,34 @@ def draw_chart(avg_pulse1, avg_pulse2, avg_pulse3, avg_pulse4):
 
 
 def test_measure_pulse():
-    pass
+    pulse1, pulse2, pulse3 = read_pulse()
+    measure_pulse(pulse1, pulse2, pulse3)
+    assert pulse1 >= 0 and pulse2 >= 0 and pulse3 >= 0
 
-draw_chart(60,50,100,80)
+def test_abnormal() :
+    print("hour 1")
+    pulse1, pulse2, pulse3 = read_pulse()
+    avg_hour1 = measure_pulse(pulse1, pulse2, pulse3)
+    print("hour 2")
+    pulse1, pulse2, pulse3 = read_pulse()
+    avg_hour2 = measure_pulse(pulse1, pulse2, pulse3)
+    print("hour 3")
+    pulse1, pulse2, pulse3 = read_pulse()
+    avg_hour3 = measure_pulse(pulse1, pulse2, pulse3)
+    print("hour 4")
+    pulse1, pulse2, pulse3 = read_pulse()
+    avg_hour4 = measure_pulse(pulse1, pulse2, pulse3)
+
+    min_level = 60
+    max_level = 90
+    hour1 = avg_hour1 >= min_level and avg_hour1 <= max_level
+    hour2 = avg_hour2 >= min_level and avg_hour2 <= max_level
+    hour3 = avg_hour3 >= min_level and avg_hour3 <= max_level
+    hour4 = avg_hour4 >= min_level and avg_hour4 <= max_level
+
+    result = abnormal(avg_hour1,avg_hour2,avg_hour3,avg_hour4)
+    if hour1 and hour2 and hour3 and hour4:
+        assert result == None
+
+
+
