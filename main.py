e497991b55f8442d65d5e9c550e1a2f9bf907a51
tma4683 <90045981+tma4683@users.noreply.github.com>
2021-10-08 19:27:03 +0400

    completed the code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pytest
 import turtle
 def read_pulse():
-    pulse1 = int(input("enter first pulse "))
-    pulse2 = int(input("enter second pulse "))
-    pulse3 = int(input("enter third pulse "))
+    pulse1 = int(input("enter first pulse :"))
+    pulse2 = int(input("enter second pulse :"))
+    pulse3 = int(input("enter third pulse :"))
     return pulse1, pulse2, pulse3
 
 def measure_pulse(pulse_1,pulse_2,pulse_3):
@@ -31,44 +31,41 @@ def warning_doctor(abnormal_hour):
 def draw_chart(avg_pulse1, avg_pulse2, avg_pulse3, avg_pulse4):
 
     t = turtle.Turtle()
-#pulse1
-    t.left(45)
+    #pulse1
+    t.left(90)
     t.forward(avg_pulse1)
-    t.right(45)
+    t.right(90)
+    t.forward(30)
+    t.right(90)
+    t.forward(avg_pulse1)
+    t.right(180)
     #pulse2
-    if avg_pulse2 < avg_pulse1 :
-        t.right(45)
-        t.forward(avg_pulse2)
-        t.left(45)
-    elif avg_pulse2 > avg_pulse1 :
-        t.left(45)
-        t.forward(avg_pulse2)
-        t.right(45)
-
+    t.forward(avg_pulse2)
+    t.right(90)
+    t.forward(30)
+    t.right(90)
+    t.forward(avg_pulse2)
+    t.right(180)
     #pulse3
-    if avg_pulse3 < avg_pulse2 :
-        t.right(45)
-        t.forward(avg_pulse3)
-        t.left(45)
-    elif avg_pulse3 > avg_pulse2 :
-        t.left(45)
-        t.forward(avg_pulse3)
-        t.right(45)
-
+    t.forward(avg_pulse3)
+    t.right(90)
+    t.forward(30)
+    t.right(90)
+    t.forward(avg_pulse3)
+    t.right(180)
     #pulse4
-    if avg_pulse4 < avg_pulse3 :
-        t.right(45)
-        t.forward(avg_pulse4)
-        t.left(45)
-    elif avg_pulse4 > avg_pulse3 :
-        t.left(45)
-        t.forward(avg_pulse4)
-        t.right(45)
-
+    t.forward(avg_pulse4)
+    t.right(90)
+    t.forward(30)
+    t.right(90)
+    t.forward(avg_pulse4)
+    t.right(180)
+    t.left(90)
+    t.forward(120)
 
     turtle.mainloop()
 
-
+# these are the TDD functions
 def test_measure_pulse():
     pulse1, pulse2, pulse3 = read_pulse()
     measure_pulse(pulse1, pulse2, pulse3)
@@ -103,15 +100,19 @@ def main() :
     print("hour 1")
     pulse1, pulse2, pulse3 = read_pulse()
     avg_hour1 = measure_pulse(pulse1, pulse2, pulse3)
+    print("average for hour 1 is :" + str(avg_hour1))
     print("hour 2")
     pulse1, pulse2, pulse3 = read_pulse()
     avg_hour2 = measure_pulse(pulse1, pulse2, pulse3)
+    print("average for hour 2 is :" + str(avg_hour2))
     print("hour 3")
     pulse1, pulse2, pulse3 = read_pulse()
     avg_hour3 = measure_pulse(pulse1, pulse2, pulse3)
+    print("average for hour 3 is :" + str(avg_hour3))
     print("hour 4")
     pulse1, pulse2, pulse3 = read_pulse()
     avg_hour4 = measure_pulse(pulse1, pulse2, pulse3)
+    print("average for hour 4 is :" + str(avg_hour4))
 
     result = abnormal(avg_hour1, avg_hour2, avg_hour3, avg_hour4)
     if result != None :
@@ -119,3 +120,6 @@ def main() :
     draw_chart(avg_hour1, avg_hour2, avg_hour3, avg_hour4)
 
 main()
+
+
+
